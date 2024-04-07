dbe59c6cc09873b6bf992c9f4c1b63c30b6dcae2
thieunv <thieunv@vng.com.vn>
2024-04-07 16:11:13 +0700

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pyspeedy",
+    version="0.1.0",
+    description="Fast and easy-to-use package for python developers.",
+    author="theluckygod",
+    author_email="thieu216777@gmail.com",
+    url="https://github.com/theluckygod/pyspeedy",
+    packages=find_packages(),  # Automatically find all packages in the directory
+    install_requires=[  # List any dependencies your package requires
+    ],
+)
